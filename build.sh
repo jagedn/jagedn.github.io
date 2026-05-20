ec1773246cb544457bfe4386daeea2b035c091fa
Jorge Aguilera <jorge@incsteps.com>
2026-05-20 10:04:06 +0200

    fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 npm i -g @asciidoctor/core asciidoctor-pdf
 npm install
 npm run docs:build
-asciidoctor-web-pdf   --template-require ./template.js -D docs/.vitepress/dist  *.adoc
+mkdir -p docs/.vitepress/dist
+asciidoctor-web-pdf   --template-require ./template.js -D docs/.vitepress/dist  cv.adoc
